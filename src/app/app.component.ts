12adae8f2874105151ddc88316dcda4ecf059f1c
Tony H. <tony.hirschligau@googlemail.com>
2025-05-29 09:17:30 +0200

    create why-me component, html structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { WhyMeComponent } from './why-me/why-me.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HeroSectionComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HeroSectionComponent, WhyMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
