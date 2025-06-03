d1350f9b46a426851dc19ca449c1617bfc8a9369
Tony H. <tony.hirschligau@googlemail.com>
2025-06-03 10:30:14 +0200

    my projects component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { WhyMeComponent } from './why-me/why-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HeroSectionComponent, WhyMeComponent, MySkillsComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HeroSectionComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
