ef5807e939fc2331d3f224eb50cdaad74d652494
Tony H. <tony.hirschligau@googlemail.com>
2025-06-03 10:53:32 +0200

    images upload, single project added, object added

--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { ProjectComponent } from './project/project.component';
 
+export interface Project {
+  name: string;
+  includedSkills: string;
+  description: string;
+  image: string;
+}
 @Component({
   selector: 'app-my-projects',
-  imports: [],
+  imports: [ProjectComponent],
+  standalone: true,
   templateUrl: './my-projects.component.html',
-  styleUrl: './my-projects.component.scss'
+  styleUrl: './my-projects.component.scss',
 })
 export class MyProjectsComponent {
-
+  projects: Project[] = [
+    {
+      name: 'El-Pollo-Loco',
+      includedSkills: 'JavaScript | HTML | CSS',
+      description:
+        'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      image: '/assets/images/Pollo loco.png',
+    },
+    {
+      name: 'JOIN',
+      includedSkills: 'Angular | TypeScript | HTML | CSS | Firebase',
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+      image: '/assets/images/angular.png',
+    },
+  ];
 }
---
+++ src/app/my-projects/project/project.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-project',
+  imports: [],
+  standalone: true,
+  templateUrl: './project.component.html',
+  styleUrl: './project.component.scss'
+})
+export class ProjectComponent {
+
+}
