8791c8c8588f3153088512fc4595bd983b787f18
Tony H. <tony.hirschligau@googlemail.com>
2025-06-04 18:32:11 +0200

    logic forprojects, animation in why me

--- src/app/why-me/why-me.component.ts
+++ src/app/why-me/why-me.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-why-me',
   imports: [],
@@ -8,9 +10,5 @@ import { Component } from '@angular/core';
   styleUrl: './why-me.component.scss'
 })
 export class WhyMeComponent {
-
-//   changeImg(){
-//     let container = document.getElementById('remote-container') as HTMLDivElement;
-//     x.style.display = "none";
-//   }
+hovered = false;
 }
