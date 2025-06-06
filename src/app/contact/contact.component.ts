f6e8304e8875efb75e85828cf3172214dadbf47a
Tony H. <tony.hirschligau@googlemail.com>
2025-06-06 13:49:42 +0200

    checkbox with submit button linked

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 let email: string;
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
   email = 'tony.hirschligau@googlemail.com'
+  privacyAccepted: boolean = false;
+  
+  
 }
+
+
