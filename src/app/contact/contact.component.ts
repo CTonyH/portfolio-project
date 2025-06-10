6228691212523c7b3a0be85e13c7d3738d78f543
Tony H. <tony.hirschligau@googlemail.com>
2025-06-10 19:15:54 +0200

    ts validation for form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 let email: string;
-
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-  email = 'tony.hirschligau@googlemail.com'
+  email = 'tony.hirschligau@googlemail.com';
+
   privacyAccepted: boolean = false;
-  
-  
+  @ViewChild('invalid-name') containerRef!: ElementRef;
+  nameInput: string = '';
+
+constructor(private renderer: Renderer2){
+
 }
 
-
+  getInputData() {
+    if (this.nameInput.length < 3) {
+      this.renderer.setStyle(this.containerRef.nativeElement, 'display', 'block');
+    } else {
+    }
+  }
+}
