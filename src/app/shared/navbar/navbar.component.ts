89c61354018317bd6dc2fe73e2b8483d8e5545c3
Tony H. <tony.hirschligau@googlemail.com>
2025-07-24 10:17:42 +0200

    added responsive, changed styling bg, max-width

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   standalone: true,
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  constructor(private viewportScroller: ViewportScroller) {
+    this.viewportScroller.setOffset([0, 100]);
+  }
+  isMobileOpen: boolean = false;
 
+  setAnchorTo(anchor: string): void {
+    this.viewportScroller.scrollToAnchor(anchor);
+  }
+
+  closeMobileMenu(): void {
+    this.isMobileOpen = false;
+  }
+
+  openMobileMenu(): void {
+    if (this.isMobileOpen === false) {
+      this.isMobileOpen = true;
+    } else {
+      this.isMobileOpen = false;
+    }
+  }
 }
