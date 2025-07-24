4a9769283f69b009448bc5e47eb807f09ba20e26
Tony H. <tony.hirschligau@googlemail.com>
2025-07-24 10:19:42 +0200

    added logic for projects reverse view, fixed button view, fixed some styling

--- src/app/my-projects/project/project.component.ts
+++ src/app/my-projects/project/project.component.ts
@@ -1,15 +1,29 @@
 import { Component, Input } from '@angular/core';
 import { Project } from '../my-projects.component';
 
-
-
 @Component({
   selector: 'app-project',
   standalone: true,
   imports: [],
   templateUrl: './project.component.html',
-  styleUrl: './project.component.scss'
+  styleUrl: './project.component.scss',
 })
 export class ProjectComponent {
   @Input() project!: Project;
+
+  goToGitHub(projectName: string): void {
+    if (projectName === 'JOIN') {
+      window.open('https://github.com/CTonyH/join.git', '_blank');
+    } else if (projectName === 'El-Pollo-Loco') {
+      window.open('https://github.com/CTonyH/el-pollo-loco.git', '_blank');
+    }
+  }
+
+  goToLiveDemo(projectName: string): void {
+    if (projectName === 'JOIN') {
+      window.open('https://join.tony-hirschligau.de', '_blank');
+    } else if (projectName === 'El-Pollo-Loco') {
+      window.open('https://el-pollo-loco.tony-hirschligau.de/', '_blank');
+    }
+  }
 }
