dfc20126a4d1655783a49639035de57103c6ff66
Tony H. <tony.hirschligau@googlemail.com>
2025-07-28 17:07:37 +0200

    added logic for back button

--- src/app/shared/legal-notice/legal-notice.component.ts
+++ src/app/shared/legal-notice/legal-notice.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-policy',
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterModule, RouterLink],
   standalone: true,
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss',
 })
-export class LegalNoticeComponent {}
+export class LegalNoticeComponent {
+  constructor(private router: Router) {}
+     goBack(): void {
+    this.router.navigate(['/']).then(() => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+  }
+}
