c3bf8041cc01662209323b93037270bace43f6ff
Tony H. <tony.hirschligau@googlemail.com>
2025-07-28 10:39:29 +0200

    added translation & translationkeys, responsive styles, styles

---
+++ src/app/shared/legal-notice/legal-notice.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-legal-policy',
+  imports: [TranslateModule],
+  standalone: true,
+  templateUrl: './legal-notice.component.html',
+  styleUrl: './legal-notice.component.scss',
+})
+export class LegalNoticeComponent {}
