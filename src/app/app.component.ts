16485195d402ecc38945a78195d3fe24b70728ec
Tony H. <tony.hirschligau@googlemail.com>
2025-07-28 10:36:20 +0200

    styling width, added translation to app routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,41 @@ import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { WhyMeComponent } from './why-me/why-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { MyProjectsComponent } from './my-projects/my-projects.component';
-import { MyFeedbacksComponent } from "./my-feedbacks/my-feedbacks.component";
+import { MyFeedbacksComponent } from './my-feedbacks/my-feedbacks.component';
 import { ContactComponent } from './contact/contact.component';
-import { ViewportScroller } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HeroSectionComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, MyFeedbacksComponent, ContactComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent,
+    HeroSectionComponent,
+    WhyMeComponent,
+    MySkillsComponent,
+    MyProjectsComponent,
+    MyFeedbacksComponent,
+    ContactComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio-project';
 
-  
-}
+  constructor(private translate: TranslateService) {
+    // Standard-Sprache setzen
+    translate.setDefaultLang('en');
+    translate.use('en');
 
+    // CSS-Klasse für Sprache setzen
+    document.body.className = 'lang-en';
+
+    // Auf Sprachwechsel hören
+    translate.onLangChange.subscribe((event) => {
+      document.body.className = `lang-${event.lang}`;
+    });
+  }
+}
